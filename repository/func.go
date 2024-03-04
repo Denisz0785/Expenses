c02dbae9db7650d1543a171ba79a11f7cea60faf
Denis Zverev <zverev.denisa7@yandex.ru>
2024-03-04 18:41:59 +0300

    added  descriptions of function in func.go

--- repository/func.go
+++ repository/func.go
@@ -9,6 +9,7 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
+// GetExpenseType gets one row of type of expenses from DB by name
 func GetExpenseType(conn *pgx.Conn, name string) *string {
 
 	var type_exp string
@@ -22,6 +23,7 @@ func GetExpenseType(conn *pgx.Conn, name string) *string {
 	return &type_exp
 }
 
+// GetManyRows gets all rows of type of expenses from DB by name
 func GetManyRows(conn *pgx.Conn, name string) []string {
 	rows, _ := conn.Query(context.Background(), "SELECT type_expenses from expense_type, users where expense_type.users_id=users.id and users.name=$1", name)
 	numbers, err := pgx.CollectRows(rows, pgx.RowTo[string])
@@ -33,6 +35,7 @@ func GetManyRows(conn *pgx.Conn, name string) []string {
 
 }
 
+// AddValuesDB insert row to the table user
 func AddValuesDB(conn *pgx.Conn) error {
 
 	commandTag, err := conn.Exec(context.Background(), "Insert into users(name,surname,login,pass,email) VALUES ('kolya','Bon','spiman','1243','er1@23.ru')")
@@ -46,6 +49,7 @@ func AddValuesDB(conn *pgx.Conn) error {
 	return err
 }
 
+// ConnDB connects to DB
 func ConnDB(myurl string) *pgx.Conn {
 	conn, err := pgx.Connect(context.Background(), os.Getenv(myurl))
 	if err != nil {
